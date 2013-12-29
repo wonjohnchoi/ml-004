53f08e227e81f7bb9fc81817441e621ee676098c
Wonjohn Choi <wonjohn.choi@gmail.com>
2013-12-29 04:51:34 -0800

    Finished 1.

--- mlclass-ex2-004/mlclass-ex2/costFunction.m
+++ mlclass-ex2-004/mlclass-ex2/costFunction.m
@@ -21,7 +21,9 @@ grad = zeros(size(theta));
 %
 
 
-
+J = 1/m*sum(-y.*log(sigmoid(X*theta)).-(1.-y).*log(1-sigmoid(X*theta)));
+diff = sigmoid(X*theta).-y;
+grad = 1/m*sum([diff diff diff].*X);
 
 
 
